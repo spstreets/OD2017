a9474a972f08a515362be177546a431c0013c94c
Lucas <lucasccdias0@gmail.com>
2022-01-07 17:54:15 -0300

    od_jitter running (need to check)

--- trips-simulation.R
+++ trips-simulation.R
@@ -3,6 +3,7 @@ library(tidyverse)
 
 piggyback::pb_download()
 
+OD_2017_v1 = readRDS("./OD_2017_v1.Rds")
 centro_expandido = st_read("./centro_expandido.geojson")
 zonas_od = readRDS("./zonas_od.Rds")
 
@@ -16,8 +17,43 @@ zonas_od_centro = zonas_od %>%          # sf_use_s2(FALSE)
   filter(dentro_centro == TRUE)
 
 viagens_centro = OD_2017_v1 %>%
-  filter((zona_o %in% unique(zonas_od_centro$NumeroZona)) &
-         (zona_d %in% unique(zonas_od_centro$NumeroZona))
-        )
+  select(zona_o, zona_d, modoprin, fe_via) %>%
+  filter(
+    (!is.na(modoprin)) &
+    (zona_o %in% unique(zonas_od_centro$NumeroZona)) &
+    (zona_d %in% unique(zonas_od_centro$NumeroZona))
+    ) %>%
+  mutate(
+    mode_ab_streets = case_when(
+      modoprin %in% 1:6 ~ "public",
+      modoprin == 16 ~ "foot",
+      modoprin == 9 | modoprin == 10 ~ "car",
+      modoprin %in% c(7, 8, 11, 12, 13, 14, 17) ~ "other",
+      modoprin == 15 ~ "bike"
+    )
+  ) %>%
+  group_by(zona_o, zona_d, mode_ab_streets) %>%
+  summarise(trips = round(sum(fe_via))) %>%          # round to avoid decimals (sampling weights here...)
+  ungroup() %>%
+  pivot_wider(names_from = mode_ab_streets,
+              values_from = trips) %>%
+  replace(is.na(.), 0) %>%
+  mutate(all = public + foot + car + other + bike) %>%
+  rename(geo_code1 = zona_o,
+         geo_code2 = zona_d) %>%
+  mutate(geo_code1 = as.character(geo_code1),
+         geo_code2 = as.character(geo_code2)
+         )
 
-saveRDS(viagens_centro, "./trips-simulation.Rds")
+zonas_od_centro = zonas_od %>%
+  filter(NumeroZona %in% unique(zonas_od_centro$NumeroZona)) %>%
+  st_transform("WGS84") %>%
+  rename(InterZone = NumeroZona) %>%
+  mutate(InterZone = as.character(InterZone))
+
+write_csv(viagens_centro, "./od_sp_center.csv")
+st_write(zonas_od_centro, "./zones_sp_center.geojson", append = FALSE)
+
+system("odjitter --od-csv-path ./od_sp_center.csv --zones-path ./zones_sp_center.geojson --max-per-od 50000 --output-path result.geojson
+")
+
