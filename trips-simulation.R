e09a99d4f925596686c6df9faa4f2eceadac8f9b
Robin Lovelace <rob00x@gmail.com>
2022-01-12 23:09:40 +0000

    Add code for scenarios #17

--- trips-simulation.R
+++ trips-simulation.R
@@ -1,8 +1,10 @@
 library(sf)
 library(tidyverse)
 library(stplanr)
-
-piggyback::pb_download()
+library(tmap)
+tmap_mode("view") # interactive maps
+# piggyback::pb_download() # download data files
+library(pct)
 
 OD_2017_v1 = readRDS("./OD_2017_v1.Rds")
 centro_expandido = st_read("./centro_expandido.geojson")
@@ -55,10 +57,16 @@ zonas_od_centro = zonas_od %>%
 write_csv(viagens_centro, "./od_sp_center.csv")
 st_write(zonas_od_centro, "./zones_sp_center.geojson", append = FALSE)
 
-system("odjitter --od-csv-path ./od_sp_center.csv --zones-path ./zones_sp_center.geojson --max-per-od 50000 --output-path result.geojson
-")
+
+system("odjitter --od-csv-path ./od_sp_center.csv --zones-path ./zones_sp_center.geojson --max-per-od 50000 --output-path result.geojson")
+
+# TODO: sample from the road network, e.g. with:
+# system("odjitter --od-csv-path ./od_sp_center.csv --zones-path ./zones_sp_center.geojson --subpoints-path osm_network.geojson --max-per-od 50000 --output-path result.geojson")
 
 od_jittered = sf::read_sf("result.geojson")
+od_jittered %>%
+  top_n(1000, all) %>%
+  qtm()
 # routes_fast = route(l = od_jittered, route_fun = cyclestreets::journey)
 #
 # # These routes failed: 1, 274, 424, 577, 609, 787, 943, 1197, 1285, 1349, 1385, 2025, 2092, 2230, 2336, 2356, 2369, 2371, 2485, 2490, 2571, 2811, 3009, 3281, 3285, 3314, 3436, 3446, 3639, 3683, 3809, 3977, 4032, 4063, 4111, 4672, 4723, 4727, 4729, 4835, 4842, 4872, 4920, 4937, 4965, 4966, 4975, 5059, 5167, 5234, 5589, 5595, 5629, 5651, 5754, 5841, 5914, 5976, 5978, 5983, 5984, 6022, 6247, 6328, 6497, 6499, 6518, 6818, 6925, 6938
@@ -67,11 +75,50 @@ od_jittered = sf::read_sf("result.geojson")
 # piggyback::pb_upload("routes_fast.geojson")
 # piggyback::pb_download_url("routes_fast.geojson")
 routes_fast = sf::read_sf("routes_fast.geojson")
+names(routes_fast)
 
 # After that: group the routes by unique origin and destination and calculate the scenarios, e.g.
 # building on this:
 #   https://github.com/ITSLeeds/pct/blob/1bc8b202b2fc9d1436b973bf97523777adca9523/data-raw/training-dec-2021.Rmd#L471
 
+routes_fast_active = routes_fast %>%
+  group_by(geo_code1, geo_code2) %>%
+  mutate(
+    rf_dist_km = length,
+    rf_avslope_perc = mean(gradient_smooth),
+    dist_bands = cut(x = rf_dist_km, breaks = c(0, 1, 3, 6, 10, 15, 20, 30, 1000), include.lowest = TRUE),
+    foot_increase_proportion = case_when(
+      # specifies that 50% of car journeys <1km in length will be replaced with walking
+      rf_dist_km < 1 ~ 0.5,
+      # specifies that 10% of car journeys 1-2km in length will be replaced with walking
+      rf_dist_km >= 1 & rf_dist_km < 2 ~ 0.1,
+      TRUE ~ 0
+    ),
+    # Specify the Go Dutch scenario we will use to replace remaining car trips with cycling
+    bike_increase_proportion = uptake_pct_godutch_2020(
+      distance = rf_dist_km,
+      gradient = rf_avslope_perc
+    ),
+    # Make the changes specified above
+    car_reduction = car * foot_increase_proportion,
+    car = car - car_reduction,
+    foot = foot + car_reduction,
+    car_reduction = car * bike_increase_proportion,
+    car = car - car_reduction,
+    bike = bike + car_reduction
+  )
+
+active_results = routes_fast_active %>%
+  sf::st_drop_geometry() %>%
+  dplyr::select(dist_bands, car, other, public_transport, bicycle, foot) %>%
+  tidyr::pivot_longer(cols = matches("car|other|publ|cy|foot"), names_to = "mode") %>%
+  mutate(mode = factor(mode, levels = c("car", "other", "public_transport", "bicycle", "foot"), ordered = TRUE)) %>%
+  group_by(dist_bands, mode) %>%
+  summarise(Trips = sum(value))
+g2 = ggplot(active_results) +
+  geom_col(aes(dist_bands, Trips, fill = mode)) +
+  scale_fill_manual(values = col_modes) + ylab("Trips")
+g2
 
 # After that: group the routes by unique origin and destination and calculate the scenarios, e.g.
 # building on this:
